bf62b2a96f6b80688e7a42ad091f2d929a9b34de
Nickolas Tushin <kovcheg.studio.2014@gmail.com>
2022-12-08 16:01:42 +0300

    add ex1

---
+++ ex1/Program.cs
@@ -0,0 +1,54 @@
+﻿
+// Первая группа методов которые 
+// ничего не возвращают и не принимают
+// Вид 1
+void Method1 ()
+{
+Console.WriteLine("Автор ...");
+}
+//Method1(); // вызвов метода !если без() будет ошибка!
+
+// Вид 2
+void Method2(string msg) // Ничего не возвращает но может
+                         // принимать какие-то аргументы
+{
+Console.WriteLine(msg);
+}
+//Method2("Текст сообщения ");
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+while (i < count)
+{
+
+Console.WriteLine (msg) ;
+i++;
+}
+}
+//Method21(msg:"Текст",count: 4);
+//Method21(count: 4, msg: "новый текст");
+
+// Вид 3 .    Что-то возвращают но ничего не принимают
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+//int year = Method3();
+//Console.WriteLine(year);
+
+// Вид 4 !!!Что-то принимают и что-то возвращают!!!
+string Method4(int count,string text)
+{
+int i =0;
+string result = String.Empty;// пустая строка
+while (i<count)
+{
+result = result+text;
+    i++;
+}
+return result;
+}
+string res = Method4(10,"Dunkan ");
+Console.WriteLine(res);
+
